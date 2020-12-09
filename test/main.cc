9eabb6f111bc26337a9bb3947479d05854cb37ce
liangjun.cao <yq_caoforever@163.com>
2020-12-09 18:31:49 +0800

    add first test framework

--- test/main.cc
+++ test/main.cc
@@ -1,17 +1,35 @@
 #include <iostream>
+#include "gtest/gtest.h"
 #include "stack.h"
 using namespace std;
 void test_stack();
-int main() {
+
+class Template_Test : public ::testing::Test{
+private:
+
+protected:
+    virtual void SetUpTestCase();
+    virtual void TearDownTestCase();
+};
+
+void Template_Test::SetUpTestCase() {
 
 }
+void Template_Test::TearDownTestCase(){
+
+} 
 
 void test_stack() {
   Stack<int> s;
   s.push(3);
+  EXPECT_EQ(1,1);
   int b;
   s.pop(b);
   cout << "b: " << b <<endl;
   cout << "stack's size : " << s.size() << endl;
   cout << __FILE__<< endl;
+}
+
+int main(int argc, char** argv) {
+    ::testing::InitGoogleTest(&argc, argv);
 }
