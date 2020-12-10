b075150e12d6e8b14dea7ffa12a5e0fbee3facd1
liangjun.cao <yq_caoforever@163.com>
2020-12-10 15:34:38 +0800

    fix a syntax error in test frame

--- test/main.cc
+++ test/main.cc
@@ -2,31 +2,29 @@
 #include "gtest/gtest.h"
 #include "stack.h"
 using namespace std;
-void test_stack();
 
-class Template_Test : public ::testing::Test{
+class Template_Test : public ::testing::Test {
 private:
-
 protected:
-    virtual void SetUpTestCase();
-    virtual void TearDownTestCase();
+    virtual void SetUp();
+    virtual void TearDown();
 };
 
-void Template_Test::SetUpTestCase() {
-
+void Template_Test::SetUp() {
 }
-void Template_Test::TearDownTestCase() {
-
-} 
-TEST_F(Template_Test,test_stack) {
+void Template_Test::TearDown() {
+}
+// ---------------------------------------Test Case-----------------------------------
+TEST_F(Template_Test, test_stack) {
   Stack<int> stack;
   int ele = 3;
   stack.push(3);
   int tmp;
-  s.pop(tmp);
+  stack.pop(tmp);
   EXPECT_EQ(ele, tmp);
 }
 
 int main(int argc, char** argv) {
-    ::testing::InitGoogleTest(&argc, argv);
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
 }
